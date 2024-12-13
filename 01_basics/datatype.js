0c77200c65b0f18419c58a5e66c0ecbb054d2528
Deepika sahu <147903794+dpkasahu@users.noreply.github.com>
2024-12-13 16:44:26 +0530

    Create datatype.js

---
+++ 01_basics/datatype.js
@@ -0,0 +1,13 @@
+let score=null
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let value=Number(score)
+console.log(typeof(value));
+console.log(value);
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1; false=>0
+
