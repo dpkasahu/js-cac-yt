c9ce7a61b39eff5b5ed05b31010a2e086b695d86
Deepika sahu <147903794+dpkasahu@users.noreply.github.com>
2025-01-03 20:28:52 +0530

    Create summaryDatatype.js

---
+++ 01_basics/summaryDatatype.js
@@ -0,0 +1,27 @@
+//primitive and non primitive(2 types)
+// primitive 7 categories
+//string,number,boolean,null,undefined,symbol,BigInt datatype
+
+const score=100
+const scoreValue=100.3
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id===anotherId);  
+
+
+//--------------------------------------------------------------------------
+//Reference type aka Non primitive data type
+//Array,Objects
+
+const heros=["gugu","bapun","parth"];
+let myObj={
+  name:"Bapun",
+  age:22,
+}
+
+const myFun=function(){
+  console.log("Hello Gugu");
+}  
+
+    
+//-----------------------------------------------------------------------------
