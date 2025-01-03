db3374292bb55096b1fea8d0582fbc3bb075d0c4
Deepika sahu <147903794+dpkasahu@users.noreply.github.com>
2025-01-03 16:23:01 +0000

    create config

---
+++ 01_basics/stackandheap.js
@@ -0,0 +1,20 @@
+//stack(primitive),heap(non primitive)
+
+let myYoutube="Deepikadotcom"
+let anotherName=myYoutube
+anotherName="chaiaurcode"
+console.log(myYoutube);
+console.log(anotherName);
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//HEAP MEMORY
+
+let userOne = {
+    email:"user@google.com",
+    upi:"user@ybl"
+}
+let userTwo = userOne
+userTwo.email="deepika@google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
+
