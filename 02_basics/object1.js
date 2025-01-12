f931f158d48aded1399b80a7d346ed9f7639ff46
Deepika sahu <147903794+dpkasahu@users.noreply.github.com>
2025-01-12 13:45:57 +0000

    object

--- 02_basics/object1.js
+++ 02_basics/object1.js
@@ -1,4 +1,4 @@
-// singleton
+// singleton {cant access key value when it have two names}
 // Object.create
 
 // object literals
@@ -7,16 +7,30 @@ const mySym = Symbol("key1")
 
 
 const JsUser = {
-    name: "Hitesh",
-    "full name": "Hitesh Choudhary",
+    name: "bapun",
+    "full name": "Bapun houdhary",
     [mySym]: "mykey1",
-    age: 18,
-    location: "Jaipur",
-    email: "hitesh@google.com",
+    age: 24,
+    location: "Bhadrak",
+    email: "bapun@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
-console.log(JsUser.email)
-console.log(JsUser["email"])
-console.log(JsUser["full name"])
-console.log(JsUser[mySym])
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email="dpkasahu@dy.com"
+// Object.freeze(JsUser)
+JsUser.email="dpkasahu@db.com"
+// console.log(JsUser);
+
+JsUser.greet=function(){
+    console.log("Hello Deepika");
+}
+JsUser.greettwo=function(){
+    console.log("Hello Deepika, ${this.name}");
+}
+
+
