d8084aa85db6de98697fdb5806802d4fd3eebb1b
Deepika Sahu <147903794+dpkasahu@users.noreply.github.com>
2025-03-15 22:38:05 +0530

    Update dateAndTime.js

--- 01_basics/dateAndTime.js
+++ 01_basics/dateAndTime.js
@@ -40,6 +40,14 @@ let ms = Date.now();
 // You cannot use it on a date object like myDate.now().
 
 // The syntax is always Date.now().
+const d = new Date("January 01, 2025");
+d.setFullYear(2020);
 
-
-
+// setDate()	Set the day as a number (1-31)
+// setFullYear()	Set the year (yyyy)
+// setHours()	Set the hour (0-23)
+// setMilliseconds()	Set the milliseconds (0-999)
+// setMinutes()	Set the minutes (0-59)
+// setMonth()	Set the month (0-11)
+// setSeconds()	Set the seconds (0-59)
+// setTime()	Set the time (milliseconds since January 1, 1970)
