30f946036013bae22d7d0e497dcd778970030739
Deepika Sahu <147903794+dpkasahu@users.noreply.github.com>
2025-03-15 20:50:49 +0530

    Updated code

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -16,20 +16,20 @@ const gameName = new String('bapun-ykp-com')
 console.log(gameName.charAt(2)); //output:p
 console.log(gameName.indexOf('p')); //output: 2
 
-const newString = gameName.substring(0, 4)
+const newString = gameName.substring(0, 4) // no negative value
 console.log(newString); // bapu
 
-const anotherString = gameName.slice(-8, 4)
-console.log(anotherString);
+const anotherString = gameName.slice(-13, 4)
+console.log(anotherString); // bapu
 
 const newStringOne = "   deepika    "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+console.log(newStringOne); //   deepika    
+console.log(newStringOne.trim());//deepika
 
 const url = "https://dpka.com/bapun%20deepikasahu"
 
-console.log(url.replace('%20', '-'))
+console.log(url.replace('%20', '-')) // https://dpka.com/bapun-deepikasahu
 
-console.log(url.includes('prusty'))
+console.log(url.includes('prusty'))//false
 
-console.log(gameName.split('-'));
+console.log(gameName.split('-')); // [ 'bapun', 'ykp', 'com' ]
