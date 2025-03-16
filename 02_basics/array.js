c83a65f1cc568ee046b6a1f68bc2a476e3d1beb5
Deepika Sahu <147903794+dpkasahu@users.noreply.github.com>
2025-03-16 12:00:49 +0530

    Update array.js

--- 02_basics/array.js
+++ 02_basics/array.js
@@ -14,7 +14,7 @@ console.log(myArr2[2])
 
 // Array methods
 
-myArr.push(6)
+myArr.push(6) // Adds a new element
 myArr.push(7)
 myArr.pop()
 
@@ -47,3 +47,40 @@ console.log(myn2);
 // converts an array to a string 
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
 document.getElementById("demo").innerHTML = fruits.toString();
+// ++++++++++++++++=keypoints+++++++++++++++++++++++
+// In JavaScript, arrays use numbered indexes.  
+// In JavaScript, objects use named indexes.
+
+// Create an array with one element:
+const points = [40];
+
+// Basic Array Methods
+Array length 
+Array toString() // converts an array to a string
+Array at() //  returns an indexed element from an array
+Array join() //  joins all array elements into a string , fruits.join(" * ");
+Array pop() // Popping items out of an array, removes the last element from an array
+Array push() // pushing items into an array , adds a new element to an array (at the end)
+Array shift() // removes the first array element and "shifts" all other elements to a lower index.
+Array unshift() // adds a new element to an array (at the beginning), and "unshifts" older elements:
+Array delete() // Using delete() leaves undefined holes in the array.
+Array concat() //method creates a new array by merging (concatenating) existing arrays
+// The concat() method does not change the existing arrays. It always returns a new array.
+// The concat() method can take any number of array arguments.
+const myGirls = ["Cecilie", "Lone"];
+const myBoys = ["Emil", "Tobias", "Linus"];
+const myChildren = myGirls.concat(myBoys);
+Array copyWithin() // copies array elements to another position in an array
+// The copyWithin() method overwrites the existing values.
+// The copyWithin() method does not add items to the array.
+// The copyWithin() method does not change the length of the array.
+Array flat() // creates a new array with sub-array elements concatenated to a specified depth
+
+// The flatMap() method first maps all elements of an array and then creates a new array by flattening the array.
+//  const myArr = [1, 2, 3, 4, 5, 6];
+// const newArr = myArr.flatMap(x => [x, x * 10]);
+//1,10,2,20,3,30,4,40,5,50,6,60(output)
+
+Array splice() //adds new items to an array
+Array toSpliced() //The difference between the new toSpliced() method and the old splice() method is that the new method creates a new array, keeping the original array unchanged, while the old method altered the original array
+Array slice()//slices out a piece of an array into a new array
