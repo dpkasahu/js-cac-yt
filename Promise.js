a5fd660de42ec2ef7da2f05493c4640c02383f7c
Deepika Sahu <147903794+dpkasahu@users.noreply.github.com>
2025-05-25 23:07:05 +0530

    Update Promise.js

--- Promise.js
+++ Promise.js
@@ -1,1 +1,6 @@
 
+// js is a Synchronous and single threaded language. <synchronous = each operation waits for the last one to complete before executing>
+// Blockin code = block the flow of program = Sync
+// Non-Blocking code = Does not block execution = Async 
+
+  
