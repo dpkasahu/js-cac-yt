e41890d2bd753543d7f6123ce98094fd37ec80cc
Deepika Sahu <147903794+dpkasahu@users.noreply.github.com>
2025-05-26 19:49:22 +0530

    Update Callback.js

--- Callback.js
+++ Callback.js
@@ -17,3 +17,16 @@ myCalculator(5, 5, myDisplayer);
 // myDisplayer is a called a callback function.
 
 // It is passed to myCalculator() as an argument.
+////API V8 ENGINE 
+const requestUrl = 'https://api.github.com/users/hiteshchoudhary'
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', requestUrl)
+    xhr.onreadystatechange = function(){
+        console.log(xhr.readyState);
+        if (xhr.readyState === 4) {
+            const data = JSON.parse(this.responseText)
+            console.log(typeof data);
+            console.log(data.followers);
+        }
+    }
+    xhr.send();
