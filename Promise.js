e292af2c21dc48a3630c3779590ed6a4f87e2bc4
Deepika Sahu <147903794+dpkasahu@users.noreply.github.com>
2025-05-28 08:25:19 +0530

    New Promise.js

--- Promise.js
+++ Promise.js
@@ -37,6 +37,72 @@ new Promise (function(resolve,reject){
   console.log(user);
 }) 
 
+const promiseFour = new Promise(function(resolve,reject){
+  setTimeout(function () {
+    let error = true
+    if(!error){
+      resolve({username:"Deepika" ,  email:"deepika@gmail.com"})
+    }
+    else{
+      reject ('ERROR :something went wrong')
+    }
+  },1000)
+  promiseFour
+  .then((user) => {
+    concole.log(user);
+    return user.username
+  })
+  .catch(function(error) {
+    console.log(error)
+  })
+  .finally (() => console.log(" It will execute for sure"))
 
 
+  //using Async and Await
+  
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if (!error) {
+            resolve({username: "javascript", password: "123"})
+        } else {
+            reject('ERROR: JS went wrong')
+        }
+    }, 1000)
+});
+async function consumePromiseFive(){
+  try{
+    const response = await promiseFive
+    console.log(response);
+  }
+  catch(error){
+    console.log(error)
+  }
+}
+
+consumePromiseFive()
+
+  // async function getAllUsers(){
+  //   try{
+  //     const response = await fetch('https://jsonplaceholder.typicode.com/users')
+  //     const data = await response.json()
+  //     console.log(data)
+  //   }
+  //   catch(error){
+  //     console.log("E: " , error);
+  //   }
+  // }
+
+  // getAllUsers()
+  
+
+fetch('https://api.github.com/users/hiteshchoudhary'){
+.then((response) => {
+  return response.json()
+})
+.then((data) => {
+  console.log(data);
+}
+.catch((error) => console.log(error))
+                                
 
