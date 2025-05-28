fe26a744a7a8bb1b2a3aa112f79b643fbb0a859d
Deepika Sahu <147903794+dpkasahu@users.noreply.github.com>
2025-05-28 07:27:58 +0530

    Update Promise.js

--- Promise.js
+++ Promise.js
@@ -3,4 +3,40 @@
 // Blockin code = block the flow of program = Sync
 // Non-Blocking code = Does not block execution = Async 
 
-  
+
+//Type 1 by declaring the variable name like promiseOne
+const promiseOne = new Promise(function(resolve,reject){
+  setTimeout(function() => {
+    console.log('Async task is completed');
+    resolve()
+  }, 1000)
+})
+promiseOne.then(function(){
+  console.log("promise completed")
+})
+
+//Type 2 without declaring the variable name
+ new Promise(function(resolve,reject){
+   setTimeout(function(){
+     console.log("Async task 2");
+     resolve()
+   },2000)
+ })
+.then(function(){
+  console.log("Async task 2 is completed")
+})
+
+//Type 3 using resolve instead of console .log
+
+new Promise (function(resolve,reject){
+  setTimeout(function(){
+    resolve({username:"Deepika" , email: "Dpka@example.com"})
+  }, 1000)
+})
+.then(function(user){
+  console.log(user);
+}) 
+
+
+
+
